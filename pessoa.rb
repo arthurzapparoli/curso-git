bcf0e2127e3f27d58352a5eb3f01dfb13010faa8
Marcio Rodrigues <marcio.rodrigues@ortizgaming.com>
2012-08-04 15:24:18 -0300

    atualizando o curso git com minhas alteracoes

--- pessoa.rb
+++ pessoa.rb
@@ -2,7 +2,7 @@ class Pessoa
 
   attr_accessor :nome, :sobrenome, :apelido, :sexo, :idade, :data_nasc, :cidade
 
-  def,  initialize(nome, sobrenome, sexo, idade, data_nasc, cidade, apelido)
+  def,  initialize(nome, sobrenome, sexo, idade, data_nasc, cidade, apelido, endereco)
     @nome = nome
     @sobrenome = sobrenome
     @sexo = sexo
@@ -10,6 +10,7 @@ class Pessoa
     @data_nasc = data_nasc
     @cidade = cidade
     @apelido = apelido
+	@endereco = endereco
   end
 
   def diga_ola
@@ -17,6 +18,7 @@ class Pessoa
       puts "Gugu dadá"
     else
       puts "Olá #{@nome} #{@sobrenome}. Você tem #{@idade} anos."
+	  puts "seu enderço: #{endereco}"
     end
   end
 end
