edab8ee3149a2cb647a68efe1cfd55ef05c7a8c7
Arthur Zapparoli <arthur.zapparoli@codeminer42.com>
2012-08-04 11:10:54 -0300

    Adicionando sobrenome

--- pessoa.rb
+++ pessoa.rb
@@ -1,11 +1,12 @@
 class Pessoa
-  attr_accessor :nome
+  attr_accessor :nome, :sobrenome
 
-  def initialize(nome)
+  def initialize(nome, sobrenome)
     @nome = nome
+    @sobrenome = sobrenome
   end
 
   def diga_ola
-    puts "Olá #{@nome}"
+    puts "Olá #{@nome} #{@sobrenome}"
   end
 end
