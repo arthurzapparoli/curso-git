658986b7925ed05041b1d47d1492d60eb227502f
Arthur Zapparoli <arthur.zapparoli@codeminer42.com>
2012-08-04 11:31:57 -0300

    Adicionando sexo

--- pessoa.rb
+++ pessoa.rb
@@ -1,9 +1,10 @@
 class Pessoa
-  attr_accessor :nome, :sobrenome
+  attr_accessor :nome, :sobrenome, :sexo
 
-  def initialize(nome, sobrenome)
+  def initialize(nome, sobrenome, sexo)
     @nome = nome
     @sobrenome = sobrenome
+    @sexo = sexo
   end
 
   def diga_ola
